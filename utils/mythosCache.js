ae70ac657c124c88ebc4469443abe062a0d423dc
printdispatch <printdispatch@gmail.com>
2025-04-06 18:48:18 -0400

    Add files via upload

--- utils/mythosCache.js
+++ utils/mythosCache.js
@@ -1,1 +1,19 @@
 
+let personaCache = null
+let symbolCache = null
+
+export function cachePersona(p) {
+  personaCache = p
+}
+
+export function getCachedPersona() {
+  return personaCache
+}
+
+export function cacheSymbols(s) {
+  symbolCache = s
+}
+
+export function getCachedSymbols() {
+  return symbolCache
+}
