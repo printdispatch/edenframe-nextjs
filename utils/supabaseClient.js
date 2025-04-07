9f1fb1acb4f9c8ce9f6f75a73cc31c069feaf3fb
printdispatch <printdispatch@gmail.com>
2025-04-07 09:47:41 -0400

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -4,5 +4,8 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+if (supabaseUrl === 'MISSING_URL') {
+  throw new Error('Supabase URL is missing from environment variables');
+}
 
 export default supabase;
