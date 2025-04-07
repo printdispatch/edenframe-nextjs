fa42d6a47881020a5fbfcdefe2294e29d2013faf
printdispatch <printdispatch@gmail.com>
2025-04-07 04:33:15 -0400

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
 
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to Edenframe</h1>
+      <p>This is your sacred interface. Visit <a href="/DreamerDashboard">/DreamerDashboard</a> to enter your realm.</p>
+    </div>
+  )
+}
